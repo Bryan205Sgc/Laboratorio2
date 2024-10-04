0e0c73bf7803d0d6ef9d3d1ffcd97736c94a1d71
bryan205 <bryangomez19032004@gmail.com>
2024-10-03 23:19:04 -0600

    array de permisos realizada

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from config import Config
 
 app = Flask(__name__)
@@ -12,9 +12,32 @@ def login():
 def registrarse():
     return render_template('registrarse.html')
 
-@app.route('/menu', methods=['GET','POST'])
+@app.route('/menu', methods=['GET', 'POST'])
 def menu():
     return render_template('menu.html')
+
+
+@app.route('/procesar_datos', methods=['POST'])
+def procesar_datos():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        rol = request.form.get('rol')
+        permisos = [
+            '1' if request.form.get('permisos_agregar') == 'on' else '0',
+            '1' if request.form.get('permisos_eliminar') == 'on' else '0',
+            '1' if request.form.get('permisos_visualizar') == 'on' else '0',
+            '1' if request.form.get('permisos_editar') == 'on' else '0'
+        ]
+
+        #El json es solo para comprobar que si esté mandando los datos XD lo podes quitar si querés
+        return jsonify({
+            "username": username,
+            "rol": rol,
+            "permisos": permisos
+        })
+
+    return render_template('menu.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
